15ea817e883508b41eafa13f74e591b5ed4b7f74
Таня Кишинская <lolo-20@yandex.ru>
2023-03-09 23:23:14 +0300

    измение

--- src/BonusMilesService.java
+++ src/BonusMilesService.java
@@ -1,27 +1,12 @@
 public class BonusMilesService {
     public static int calculate(int price) {
-
-        int rubles = 20; // 1 бонус за рублей
-        boolean isRegistered = true;
-
         int miles;
-        if (isRegistered) {
+        if (1 > 0) {
             miles = 1;
         } else {
             miles = 0;
         }
-
-        // int  miles= isRegistered ? 1 : 0;
-
         int result = price / 20 * miles;
-
-        if (miles > 500) {
-            miles = 500;
-        }
-//        System.out.println("Стоимость билета: " + price + "\nВаш бонус составляет: " + result);
         return result;
     }
 }
-
-
-
--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,10 @@
 public class Main {
     public static void main(String[] args) {
-
+        int rubles = 20; // 1 миля за рублей
 
         BonusMilesService service = new BonusMilesService();
         int price = 10_000;
-        int miles = service.calculate(price); // должно получиться 500
+        int miles = service.calculate(price);
         System.out.println(miles);
 
     }
