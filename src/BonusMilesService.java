226ecaa1380330c02be3b69a08f8e7d32eb26c10
Таня Кишинская <lolo-20@yandex.ru>
2023-03-13 16:24:43 +0300

    испровление

--- src/BonusMilesService.java
+++ src/BonusMilesService.java
@@ -2,13 +2,8 @@ public class BonusMilesService {
     public static int calculate(int price) {
         int rubles = 20; // 1 миля за рублей
 
-        int miles;
+        int miles = 1;
 
-        if (1 > 0) {
-            miles = 1;
-        } else {
-            miles = 0;
-        }
         int result = price / 20 * miles;
         return result;
     }
